fca9637a237f0aa0b6e525f3134b26c2e89422cb
2leonardo <2leonardo.esposito@gmail.com>
2024-05-17 15:27:15 +0100

    build: add next seo props to config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 import Logo from './components/Logo/Logo';
 
 const config: DocsThemeConfig = {
+    useNextSeoProps() {
+        const { asPath } = useRouter();
+        if (asPath !== '/') {
+            return {
+                titleTemplate: '%s - Kapow Docs',
+            };
+        }
+    },
     logo: Logo,
     project: {
         link: 'https://github.com/SoBold/kapow-docs',
